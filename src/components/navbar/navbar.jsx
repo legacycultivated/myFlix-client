cc08a9d67077914329f5751107c7a3920ef84a02
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-07-20 16:24:44 -0700

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -22,21 +22,38 @@ export function Navbar({ user }) {
   };
 
   return (
-    <Navbar className="main-nav" expand="lg">
-      {/* <Container> */}
-      <Navbar.Brand className="text-light">myFlix2022</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="ml-auto">
-          {isAuth() && <Nav.Link href="/">Home</Nav.Link>}
-          {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
+    <Navbar
+      className="main-nav"
+      sticky="top"
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
+      <Container>
+        <Navbar.Brand className="navbar-logo" href="/">
+          myFlix2022
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            {isAuth() && <Nav.Link href="/">Home</Nav.Link>}
+            {isAuth() && <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>}
 
-          {isAuth() && <Button onClick={onLoggedOut}>Logout</Button>}
-          {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
-          {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
-        </Nav>
-      </Navbar.Collapse>
-      {/* </Container> */}
+            {isAuth() && (
+              <Button
+                variant="link"
+                onClick={() => {
+                  this.onLoggedOut();
+                }}
+              >
+                Logout
+              </Button>
+            )}
+            {!isAuth() && <Nav.Link href="/">Sign-in</Nav.Link>}
+            {!isAuth() && <Nav.Link href="/register">Sign-up</Nav.Link>}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }

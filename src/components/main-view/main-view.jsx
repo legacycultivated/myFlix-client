bca36b2af2f7aa39308ae483307f908a2def794b
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-07-20 09:56:42 -0700

    update movie-card

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 
 import { LoginView } from "../login-view/login-view";
+import { RegistrationView } from "../registration-view/registration-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -11,6 +12,8 @@ export class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
+      user: null,
+      registered: null,
     };
   }
   componentDidMount() {
@@ -40,13 +43,34 @@ export class MainView extends React.Component {
     });
   }
 
+  onRegistration(registered) {
+    this.setState({
+      registered,
+    });
+  }
+
   render() {
-    const { movies, selectedMovie, user } = this.state;
+    const { movies, selectedMovie, user, registered } = this.state;
 
     /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
     if (!user)
       return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
+    if (!registered)
+      return (
+        <RegistrationView
+          onRegistration={(register) => this.onRegistration(register)}
+        />
+      );
+    if (selectedMovie)
+      return (
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={(newSelectedMovie) => {
+            this.setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      );
     // Before the movies have been loaded
     if (movies.length === 0) return <div className="main-view" />;
 

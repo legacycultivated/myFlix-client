92f8076108b43976fe468723c1875370680c892c
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-07-20 10:45:05 -0700

    update login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -28,27 +28,35 @@ export function LoginView(props) {
         <Col>
           <CardGroup>
             <Card>
-              <CardHeader>Please Login</CardHeader>
-              <Form>
-                <Form.Group controlId="formUsername">
-                  <Form.Label>Username:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    onChange={(e) => setUsername(e.target.value)}
-                  />
-                </Form.Group>
+              <Card.Body>
+                <Card.Title>Please Login</Card.Title>
+                <Form>
+                  <Form.Group controlId="formUsername">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      onChange={(e) => setUsername(e.target.value)}
+                      placeholder="Please Enter Username"
+                    />
+                  </Form.Group>
 
-                <Form.Group controlId="formPassword">
-                  <Form.Label>Password:</Form.Label>
-                  <Form.Control
-                    type="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                  Submit
-                </Button>
-              </Form>
+                  <Form.Group controlId="formPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Please Enter Password"
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </Button>
+                </Form>
+              </Card.Body>
             </Card>
           </CardGroup>
         </Col>
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -30,51 +30,57 @@ export function RegistrationView(props) {
         <Col>
           <CardGroup>
             <Card>
-              <Card.Header>Please Register</Card.Header>
-              <Form>
-                <Form.Group>
-                  <Form.Label>Username:</Form.Label>
-                  <Form.Control
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    placeholder="Enter a username"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Password:</Form.Label>
-                  <Form.Control
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    minLength="8"
-                    placeholder="Enter a password at least 8 characters long"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label> Email: </Form.Label>
-                  <Form.Control
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                    placeholder="Enter email"
-                  />
-                </Form.Group>
-                <Form.Group>
-                  <Form.Label>Birthday: </Form.Label>
-                  <Form.Control
-                    type="date"
-                    value={birthday}
-                    onChange={(e) => setBirthday(e.target.value)}
-                  />
-                </Form.Group>
-                <Button variant="primary" type="submit" onClick={handleSubmit}>
-                  Register
-                </Button>
-              </Form>
+              <Card.Body>
+                <Card.Title>Please Register</Card.Title>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      required
+                      placeholder="Enter a username"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      required
+                      minLength="8"
+                      placeholder="Enter a password at least 8 characters long"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label> Email: </Form.Label>
+                    <Form.Control
+                      type="email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                      placeholder="Enter email"
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Form.Label>Birthday: </Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={birthday}
+                      onChange={(e) => setBirthday(e.target.value)}
+                    />
+                  </Form.Group>
+                  <Button
+                    variant="primary"
+                    type="submit"
+                    onClick={handleSubmit}
+                  >
+                    Register
+                  </Button>
+                </Form>
+              </Card.Body>
             </Card>
           </CardGroup>
         </Col>

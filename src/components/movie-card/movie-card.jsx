eb1b3a5a38e1f0fe0c34565f5ed91f4e02adc9de
legacycultivated <87409209+legacycultivated@users.noreply.github.com>
2022-07-20 23:16:46 -0700

    Update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,13 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import axios from "axios";
 
 import { Link } from "react-router-dom";
 
 import "./movie-card.scss";
 export class MovieCard extends React.Component {
+  addMovie(movie, user) {
+    const username = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    console.log(movie);
+    console.log(token);
+
+    axios
+      .post(
+        `https://myflix2022.herokuapp.com/users/${username}/movies/${movie._id}`,
+        {},
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+      .then((response) => {
+        this.setState({
+          user: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
   render() {
-    const { movie } = this.props;
+    const { movie, user } = this.props;
 
     return (
       <Card>
@@ -19,18 +42,34 @@ export class MovieCard extends React.Component {
           <Link to={`/movies/${movie._id}`}>
             <Button variant="link">Open</Button>
           </Link>
+          <Button
+            className="button ml-2"
+            size="sm"
+            onClick={() => {
+              this.addMovie(movie, user);
+            }}
+          >
+            Add
+          </Button>
         </Card.Body>
       </Card>
     );
   }
 }
+
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired,
+    movie: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired,
     Genre: PropTypes.shape({
       Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
     }),
-    ImagePath: PropTypes.string.isRequired,
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio: PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+    }),
   }).isRequired,
 };
